d92d77e06f844461bdf6a0721bcc63743d707f6a
valanhyr <valanhyr@outlook.es>
2026-09-12 08:43:18 +0200

    initial comit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App(){
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50">
+      <h1 className="text-2xl font-semibold">Wheel of Life — POC</h1>
+    </div>
+  )
+}
